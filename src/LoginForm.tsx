3b3f12e0e2d4dfd3aa5d3eb6cc2374e47e70352e
Mateusz <mateusz.dominiak@stxnext.pl>
2021-04-24 12:03:08 +0200

    add reactstrap and basic styling

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,51 +1,67 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import {
+  Button,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from 'reactstrap';
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch("http://localhost:3333/login", {
+    fetch('http://localhost:3333/login', {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         username: username,
         password: password,
       }),
     }).then((response) => {
       if (response.ok) {
-        alert("Success!");
+        alert('Success!');
       } else {
-        alert("Failed!");
+        alert('Failed!');
       }
     });
   };
 
   return (
-    <form className="form" onSubmit={submit}>
-      <div>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <button type="submit">Sign in</button>
-      </div>
-    </form>
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Form onSubmit={submit}>
+        <FormGroup>
+          <Label for="username">Username</Label>
+          <Input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+
+        <Button type="submit">Sign in</Button>
+      </Form>
+    </Container>
   );
 };
 
